6dc0d5f25783b1ae766acd836b1a2728dda52186
ramon96 <rfm1@live.nl>
2022-05-19 17:22:24 +0200

    loading states when searching and when the data has been received

--- src/features/youtube/youtubeComponent.js
+++ src/features/youtube/youtubeComponent.js
@@ -9,8 +9,16 @@ import {
 
 // mui components
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+
+import Box from '@mui/material/Box';
+
+import Fab from '@mui/material/Fab';
+import CheckIcon from '@mui/icons-material/Check';
+import SearchIcon from '@mui/icons-material/Search';
+import { green } from '@mui/material/colors';
+
+import CircularProgress from '@mui/material/CircularProgress';
 
 export function Playlist() {
     const dispatch = useAppDispatch();
@@ -22,13 +30,21 @@ export function Playlist() {
     // // dispatch(getPlaylist("https://www.youtube.com/playlist?list=PLrIbvYfRNoqXKmAe7EFLWaH2Gi22-GSwg"))
     // }, [dispatch]);
 
+    const submitSx = {
+        ...(playlist.status === "success" && {
+          bgcolor: green[500],
+          '&:hover': {
+            bgcolor: green[700],
+          },
+        }),
+      };
+
     const onSubmit = (data) => {
         //check if we got a valid link
         if(!data.link.includes('https://www.youtube.com/playlist?list=')) { 
             alert('Please enter a valid youtube playlist link');
             return;
         }
-
 
       dispatch(getPlaylist(data.link))
     };
@@ -37,7 +53,7 @@ export function Playlist() {
     <div>
         {/* {console.log(playlist)} */}
         <form onSubmit={handleSubmit(onSubmit)}>
-            <Stack spacing={2} direction="row">
+            <Stack spacing={2} direction="row" my={2}>
                 <TextField 
                     id="link" 
                     label="Playlist" 
@@ -45,16 +61,49 @@ export function Playlist() {
                     fullWidth
                     {...register('link')} 
                 />
-                <Button 
+                <Box sx={{ position: 'relative' }}>
+                    <Fab
+                        color="primary"
+                        type="submit"
+                        sx={submitSx}
+                    >
+                        {playlist.status === "success" ? <CheckIcon /> : <SearchIcon />}
+                    </Fab>
+                    {playlist.status === "loading" && 
+                        <CircularProgress
+                            size={68}
+                            sx={{
+                                color: green[500],
+                                position: 'absolute',
+                                top: -6,
+                                left: -6,
+                                zIndex: 1,
+                            }}
+                        />
+                    }
+                </Box>
+                {/* <Button 
                     variant="text" 
                     type="submit"
                 >
-                    Text
-                </Button>
+                    Submit
+                </Button> */}
             </Stack>
         </form>
 
         {/* TODO: maak hier een component van */}
+        {console.log(playlist.playlist)}
+        {playlist.playlist.length === 0 
+            ? (
+                <Stack my={2}>
+                    No playlist imported yet.
+                </Stack>
+            ) : (
+                <Stack my={2}>
+                    <h1>Playlist found</h1>
+                </Stack>
+            )
+        }
     </div>
     );
 }
